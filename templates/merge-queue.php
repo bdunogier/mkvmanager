fc6afb5e5f6473c673901d83f3189869521503a8
Bertrand Dunogier <bd@ez.no>
2011-01-15 14:56:01 +0100

    CSS improvements (tables + global)

--- templates/merge-queue.php
+++ templates/merge-queue.php
@@ -4,29 +4,31 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 
 <script type="text/javascript">
-var processing = false;
-function updateTable()
+mm.mergeQueue = {};
+mm.mergeQueue.processing = false;
+mm.mergeQueue.updateTable = function updateTable()
 {
-    if ( processing == true ) return;
-    processing = true;
+    if ( mm.mergeQueue.processing == true ) return;
+    mm.mergeQueue.processing = true;
     $.get(
-        '/ajax/merge-queue-status',
+        '/ajax/merge-queue/<?=$this->items?>',
         function success( r ) {
             $("#MergeQueueStatus").find("tr:gt(0)").remove();
             $("#MergeQueueStatus tr:last").after( r.html_table );
             console.log( $("#MergeQueueStatus").find("tr:gt(0)") );
-            processing = false;
-            setTimeout( 'updateTable()', 200 );
+            mm.mergeQueue.processing = false;
+            setTimeout( 'mm.mergeQueue.updateTable()', 200 );
         }, 'json' );
 }
 $(document).ready(function() {
-    updateTable();
+    mm.mergeQueue.updateTable();
 });
  </script>
 
 <h1>Merge queue status</h1>
 
 <table title="Merge queue status" id="MergeQueueStatus">
+    <thead>
     <tr>
         <th>Hash</th>
         <th>File</th>
@@ -34,4 +36,7 @@ $(document).ready(function() {
         <th>End time</th>
         <th>Progress</th>
     </tr>
+    </thead>
+    <tbody>
+    </tbody>
 </table>
