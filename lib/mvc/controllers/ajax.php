07762764dfbd1c574cf216f19c021c8cd70fad4c
Bertrand Dunogier <bd@ez.no>
2011-04-25 01:32:53 +0200

    Fixed auto-TV location when TV show doesn't exist yet

--- lib/mvc/controllers/ajax.php
+++ lib/mvc/controllers/ajax.php
@@ -424,7 +424,7 @@ class mmAjaxController extends ezcMvcController
 
             // determine best disk
             $bestFit = mmMkvManagerDiskHelper::BestTVEpisodeFit( $episodeFile->fullname, $episodeFile->fileSize );
-            if ( $bestFit['RecommendedDiskHasFreeSpace'] )
+            if ( isset( $bestFit['RecommendedDiskHasFreeSpace'] ) && $bestFit['RecommendedDiskHasFreeSpace'] )
                 $disk = $bestFit['RecommendedDisk'];
             else
                 $disk = 'VIMES';
