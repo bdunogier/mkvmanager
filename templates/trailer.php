5f28eacac35645689111fb9d66a5d129564e18ad
Bertrand Dunogier <bd@ez.no>
2011-05-30 10:41:54 +0200

    Removed trailer view

--- templates/trailer.php
+++
@@ -1,7 +0,0 @@
-<h1>Trailer for allocine movie #<?=$this->allocine_id?></h1>
-
-<ul>
-<?foreach ( $this->trailers as $trailer ):?>
-    <li><a href="<?=$trailer->url?>" title="<?=htmlentities( utf8_decode( $trailer->title ) )?>"><?=htmlentities( utf8_decode( $trailer->title ) )?></a></li>
-<?endforeach?>
-</ul>
