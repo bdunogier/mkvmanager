563aea646a5bde0ea138076bf30d955b6f378484
Arnaud Lafon <arnaud.lafon@gmail.com>
2011-11-16 19:41:35 +0100

    Fixed something already fixed (and merged) in 10395553312d94bae559b94ec840a0a70e910784.

--- templates/nfo/movie/search.php
+++ templates/nfo/movie/search.php
@@ -6,7 +6,7 @@
         <td>
             <h3><?= $result->originalTitle ?> (<?= $result->releaseYear ?>)</h3>
             <?if ( isset( $result->url_allocine ) ):?><a href="<?=$result->url_allocine?>">See on allocine.fr</a><br /><?endif?>
-            <?if ( isset( $result->url_tmdb ) ):?><a href="<?=$result->url_url_tmdb?>">See on tmdb.org</a><br /><?endif?>
+            <?if ( isset( $result->url_tmdb ) ):?><a href="<?=$result->url_tmdb?>">See on tmdb.org</a><br /><?endif?>
             <br />
             <a href="<?=$result->generateUrl?>">Generate NFO</a><br />
         </td>
