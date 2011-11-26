bf5be7ad5e390a784e3cf2d7a50036b57488902c
Bertrand Dunogier <bd@ez.no>
2011-11-26 14:18:42 +0100

    Slight refactoring of the statusbar JS

--- templates/pagelayout.php
+++ templates/pagelayout.php
@@ -9,34 +9,62 @@
     <script type="text/javascript" src="/js/jquery-1.4.2.min.js"></script>
     <script type="text/javascript" src="/js/jquery.bpopup-0.4.1.min.js"></script>
     <script type="text/javascript">
-    var mm = {};
-    mm.statusBar = {};
-    mm.statusBar.processing = false;
-    mm.statusBar.updateStatus = function updateStatus()
-    {
-        if ( mm.statusBar.processing == true ) {
-            return
+    var mm = {
+        statusBar: {
+            /**
+             * Status refresh rate, in milliseconds
+             * @var int
+             */
+            refreshRate: 1000,
+
+            /**
+             * Current request start time, in milliseconds
+             * @var int
+             */
+            requestStartTime: false,
+
+            updateStatus: function updateStatus() {
+
+                if ( mm.statusBar.requestStartTime != false ) {
+                    console.log( "Pending request detected, aborting", mm.statusBar );
+                    return;
+                }
+
+                mm.statusBar.requestStartTime = new Date().getTime();
+                console.log( "No pending request, processing" );
+                $.get(
+                    '/ajax/daemon/queue-contents/running/Merge',
+                    function success( r ) {
+                        console.log( "Success", r );
+                        var timeout;
+                        if ( r.message != 'no-operation')
+                        {
+                            $("#statuspanel").html(
+                                '<progress id="pbStatus" max="100" value="' + r.queue[0].progress + '"></progress>' +
+                                '<div id="pbStatusText">'+r.queue[0].title+'</div>');
+                            currentTime = new Date().getTime();
+
+                            if ( mm.statusBar.requestStartTime == false )
+                                timeout = mm.statusBar.refreshRate;
+                            else
+                                timeout = mm.statusBar.refreshRate - ( currentTime - mm.statusBar.requestStartTime );
+
+                        }
+                        else
+                        {
+                            $("#statuspanel").html( 'Idle' );
+                            timeout = 1000;
+                        }
+
+                        mm.statusBar.requestStartTime = false;
+
+                        console.log( "Setting timeout to " + timeout + "ms", mm.statusBar );
+                        setTimeout( 'mm.statusBar.updateStatus()', timeout );
+                    },
+                    'json'
+                );
+            }
         }
-        mm.statusBar.processing = true;
-            $.get(
-                '/ajax/daemon/queue-contents/running/Merge',
-                function success( r ) {
-                var timeout;
-                if ( r.message != 'no-operation')
-                {
-                    $("#statuspanel").html(
-                        '<progress id="pbStatus" max="100" value="' + r.queue[0].progress + '"></progress>' +
-                        '<div id="pbStatusText">'+r.queue[0].title+'</div>');
-                    timeout = 50;
-                }
-                else
-                {
-                    $("#statuspanel").html( 'Idle' );
-                    timeout = 1000;
-                }
-                mm.statusBar.processing = false;
-                setTimeout( 'mm.statusBar.updateStatus()', timeout );
-            }, 'json' );
     };
 
     $(document).ready(function() {
